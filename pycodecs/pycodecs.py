6de7ebc1b89dc3f4c5749e32f915bce499088a31
Jan <kloppjp@gmail.com>
2020-06-12 14:50:13 +0800

    Enable piping for communication with FFMPEG

--- pycodecs/pycodecs.py
+++ pycodecs/pycodecs.py
@@ -3,8 +3,9 @@ from tempfile import NamedTemporaryFile
 import imageio
 import os
 import subprocess
-from typing import Union
+from typing import Union, List
 from distutils.spawn import find_executable
+import re
 
 
 class Codec(object):
@@ -16,63 +17,83 @@ class Codec(object):
         else:
             self.default_quality = quality
 
-    def encode(self, ifile: str, ofile: str, quality: int = None):
+    def encode(self, source: Union[str, np.ndarray], target: Union[str, None] = None, quality: int = None) \
+            -> Union[None, np.ndarray]:
         raise NotImplementedError()
 
-    def decode(self, ifile: str, ofile: str):
+    def decode(self, source: Union[str, bytes], target: Union[str, None] = None) -> Union[None, np.ndarray]:
         raise NotImplementedError()
 
-    def quality_steps(self):
+    def can_pipe(self) -> bool:
         raise NotImplementedError()
 
-    def available(self):
+    def quality_steps(self) -> List[int]:
+        raise NotImplementedError()
+
+    def available(self) -> bool:
         raise NotImplementedError()
 
     def apply(self, original: Union[np.ndarray, str], quality: int = None, encoded: str = None, decoded: str = None) -> \
             (int, np.ndarray):
         channels_first = False
+        encode_to_file = encoded is not None
+        decode_to_file = decoded is not None
+        original_file = None
         if type(original) == np.ndarray:
-            original_file = NamedTemporaryFile(suffix=".png")
-            assert original.ndim in (2, 3)
-            if original.ndim == 3:
+            if original.ndim == 4:
+                if original.shape[0] != 1:
+                    raise ValueError("If a 4D ndarray is supplied, it can only have a single entry in the first "
+                                     "dimension")
+                original = original[0]
+            if original.ndim == 3:  # Check which is the channel dimension
                 if original.shape[0] == 3 and not original.shape[2] == 3:
                     original = np.transpose(original, (1, 2, 0))
                     channels_first = True
-            imageio.imwrite(original_file.name, original)
-            original_file_name = original_file.name
-        else:
-            original_file_name = original
+            if not self.can_pipe():
+                original_file = NamedTemporaryFile(suffix=".png")
+                imageio.imwrite(original_file.name, original)
+                original = original_file.name
 
-        if encoded is None:
+        encoded_file = None
+        if encoded is None and not self.can_pipe():
             encoded_file = NamedTemporaryFile(suffix=self.file_extension)
-            encoded_file_name = encoded_file.name
-        else:
-            encoded_file_name = encoded
+            encoded = encoded_file.name
 
-        if decoded is None:
+        decoded_file = None
+        if decoded is None and not self.can_pipe():
             decoded_file = NamedTemporaryFile(suffix=".png")
-            decoded_file_name = decoded_file.name
-        else:
-            decoded_file_name = decoded
+            decoded = decoded_file.name
 
         if quality is None:
             quality = self.default_quality
-        self.encode(original_file_name, encoded_file_name, quality)
-        self.decode(encoded_file_name, decoded_file_name)
 
-        encoded_size_bytes = os.stat(encoded_file_name).st_size
+        encoder_output = self.encode(original, encoded, quality)
+        if not encode_to_file and self.can_pipe():
+            encoded = encoder_output
+            encoded_size_bytes = len(encoded)
+        else:
+            encoded_size_bytes = os.stat(encoded).st_size
 
-        restored = None
-        if decoded is None:
-            restored = imageio.imread(decoded_file_name)
-            if channels_first:
-                restored = np.transpose(restored, (2, 0, 1))
+        decoder_output = self.decode(encoded, decoded)
+
+        if not decode_to_file:
+            if self.can_pipe():
+                restored = decoder_output
+            else:
+                restored = imageio.imread(decoded)
+        else:  # We don't restore if file is given (you can read it yourself)
+            restored = None
+
+        if restored is not None and channels_first:
+            restored = np.transpose(restored, (2, 0, 1))
+
+        if decoded_file is not None:
             decoded_file.close()
 
-        if encoded is None:
+        if encoded_file is not None:
             encoded_file.close()
 
-        if type(original) == np.ndarray:
+        if original_file is not None:
             original_file.close()
         return encoded_size_bytes, restored
 
@@ -92,23 +113,23 @@ class BPG(Codec):
     def available(self):
         return not find_executable("bpgenc") is None and not find_executable("bpgdec") is None
 
-    def encode(self, ifile: str, ofile: str, quality: int = None):
+    def encode(self, source: Union[str, np.ndarray], target: Union[str, None] = None, quality: int = None) \
+            -> Union[None, np.ndarray]:
         if quality is None:
             quality = self.default_quality
         subprocess.run(["bpgenc", "-m", str(self.speed), "-b", str(self.bitdepth), "-q", str(quality), "-c",
-                        self.colourspace, "-f", self.format, "-e", self.encoder, ifile, "-o", ofile])
+                        self.colourspace, "-f", self.format, "-e", self.encoder, source, "-o", target])
+        return None
 
-    def decode(self, ifile: str, ofile: str):
-        subprocess.run(["bpgdec", ifile, "-o", ofile])
+    def decode(self, source: Union[str, bytes], target: Union[str, None] = None) -> Union[None, np.ndarray]:
+        subprocess.run(["bpgdec", source, "-o", target])
+        return None
 
     def quality_steps(self):
         return [q for q in range(51, 0, -1)]
 
-
-class X265(BPG):
-
-    def __init__(self, **kwargs):
-        super(X265, self).__init__(encoder='x265', **kwargs)
+    def can_pipe(self):
+        return False
 
 
 class H265(BPG):
@@ -127,16 +148,22 @@ class WebP(Codec):
     def available(self):
         return not find_executable('cwebp') is None and not find_executable('dwebp') is None
 
-    def encode(self, ifile: str, ofile: str, quality: int = None):
+    def encode(self, source: Union[str, np.ndarray], target: Union[str, None] = None, quality: int = None) \
+            -> Union[None, np.ndarray]:
         if quality is None:
             quality = self.default_quality
-        subprocess.run(["cwebp", "-quiet", "-m", str(self.speed), "-q", str(quality), ifile, "-o", ofile])
+        subprocess.run(["cwebp", "-quiet", "-m", str(self.speed), "-q", str(quality), source, "-o", target])
+        return None
 
-    def decode(self, ifile: str, ofile: str):
-        subprocess.run(["dwebp", "-quiet", ifile, "-o", ofile])
+    def decode(self, source: Union[str, bytes], target: Union[str, None] = None) -> Union[None, np.ndarray]:
+        subprocess.run(["dwebp", "-quiet", source, "-o", target])
+        return None
 
     def quality_steps(self):
         return [q for q in range(0, 101)]
+
+    def can_pipe(self):
+        return False
 
 
 class JPEGFI(Codec):
@@ -148,16 +175,22 @@ class JPEGFI(Codec):
     def available(self):
         return True
 
-    def encode(self, ifile: str, ofile: str, quality: int = None):
+    def encode(self, source: Union[str, np.ndarray], target: Union[str, None] = None, quality: int = None) \
+            -> Union[None, np.ndarray]:
         if quality is None:
             quality = self.default_quality
-        imageio.imwrite(ofile, imageio.imread(ifile), quality=quality, optimize=True, baseline=True)
+        imageio.imwrite(target, imageio.imread(source), quality=quality, optimize=True, baseline=True)
+        return None
 
-    def decode(self, ifile: str, ofile: str):
-        imageio.imwrite(ofile, imageio.imread(ifile))
+    def decode(self, source: Union[str, bytes], target: Union[str, None] = None) -> Union[None, np.ndarray]:
+        imageio.imwrite(target, imageio.imread(source))
+        return None
 
     def quality_steps(self):
         return [q for q in range(1, 101)]
+
+    def can_pipe(self) -> bool:
+        return False
 
 
 class JPEG(Codec):
@@ -169,52 +202,144 @@ class JPEG(Codec):
     def available(self):
         return True
 
-    def encode(self, ifile: str, ofile: str, quality: int = None):
+    def encode(self, source: Union[str, np.ndarray], target: Union[str, None] = None, quality: int = None) \
+            -> Union[None, np.ndarray]:
         if quality is None:
             quality = self.default_quality
-        imageio.imwrite(ofile, imageio.imread(ifile), quality=quality, optimize=True)
+        imageio.imwrite(target, imageio.imread(source), quality=quality, optimize=True)
+        return None
 
-    def decode(self, ifile: str, ofile: str):
-        imageio.imwrite(ofile, imageio.imread(ifile))
+    def decode(self, source: Union[str, bytes], target: Union[str, None] = None) -> Union[None, np.ndarray]:
+        imageio.imwrite(target, imageio.imread(source))
+        return None
 
     def quality_steps(self):
         return [q for q in range(1, 101)]
 
+    def can_pipe(self) -> bool:
+        return False
 
-class AV1(Codec):
+
+class FFMPEG(Codec):
 
     def __init__(self, pixel_format: str = 'yuv444p', ffmpeg_path: str = None, **kwargs):
-        super(AV1, self).__init__(**kwargs)
-        self.file_extension = '.ivf'
+        super(FFMPEG, self).__init__(**kwargs)
+        self.file_extension = '.nut'
+        self.format = 'nut'
         self.pixel_format = pixel_format
+        self.codec = ''
         if ffmpeg_path is None:
             self.ffmpeg_path = 'ffmpeg'
         else:
             self.ffmpeg_path = ffmpeg_path
+        self.additional_output_commands = list()
+        self.additional_input_commands = list()
 
-    def available(self):
+    def can_pipe(self) -> bool:
+        return True
+
+    def _quality_param(self, quality: int) -> List[str]:
+        raise NotImplementedError()
+
+    def _available(self, codec_code: str):
         ffmpeg_exec = find_executable(self.ffmpeg_path)
         if ffmpeg_exec is None:
             return False
-        output = subprocess.Popen([self.ffmpeg_path, '-hide_banner', '-codecs'], stdout=subprocess.PIPE).communicate()
+        output = subprocess.Popen([self.ffmpeg_path, '-hide_banner', '-codecs'], stdout=subprocess.PIPE).communicate()[0]
+        output = output.decode().split('\n')
         for line in output:
-            if "libaom-av1" in line.decode():
+            if codec_code in line:
                 return True
         return False
 
-    def encode(self, ifile: str, ofile: str, quality: int = None):
+    def encode(self, source: Union[str, np.ndarray], target: Union[str, None] = None, quality: int = None) \
+            -> Union[None, bytes]:
         if quality is None:
             quality = self.default_quality
-        subprocess.run([self.ffmpeg_path, '-y', '-hide_banner', '-loglevel', 'panic',
-                        "-i", ifile, "-c:v", "libaom-av1", "-pix_fmt", self.pixel_format, "-crf", f"{quality}",
-                        "-b:v", "0", "-strict", "experimental", ofile])
+        source_file = source
+        if type(source) == np.ndarray:
+            source_file = "-"
+        target_file = target
+        if target is None:
+            target_file = "-"
 
-    def decode(self, ifile: str, ofile: str):
-        subprocess.run([self.ffmpeg_path, '-y', '-hide_banner', '-loglevel', 'panic',
-                        '-i', ifile, ofile])
+        cmd = [self.ffmpeg_path, '-y', '-hide_banner', '-loglevel', 'panic', '-nostats'] + \
+              self.additional_input_commands + ["-i", source_file, "-c:v", self.codec, "-pix_fmt", self.pixel_format] \
+              + self._quality_param(quality) + self.additional_output_commands + ['-f', self.format, target_file]
+        proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if type(source) == np.ndarray:
+            return proc.communicate(input=source.tobytes())[0] # if target is a file, then this will just return None
+        else:
+            return proc.communicate()[0]
+
+    def decode(self, source: Union[str, bytes], target: Union[str, None] = None) -> Union[None, np.ndarray]:
+        source_file = source
+        if type(source) == bytes:
+            source_file = "-"
+        target_file = target
+        output_spec = list()
+        if target is None:
+            target_file = "-"
+            output_spec = ['-pix_fmt', 'rgb24', '-f', 'rawvideo']
+
+        proc = subprocess.Popen([self.ffmpeg_path, '-y', '-hide_banner', '-f', self.format,
+                                '-i', source_file] + output_spec + [target_file], stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        if type(source) == bytes:
+            comm = proc.communicate(input=source)
+        else:
+            comm = proc.communicate()
+
+        if target is None:
+            h = w = 0
+            for line in comm[1].decode().split('\n'):
+                if line.strip().startswith("Stream"):
+                    for d in line.strip().split(','):
+                        match = re.search("^([0-9]{1,4})x([0-9]{1,4})$", d.strip())
+                        if match is not None:
+                            w = int(match.groups()[0])
+                            h = int(match.groups()[1])
+            if h == 0 or w == 0:
+                raise ValueError(f"Could not find image dimensions in FFMPEG info: {comm[1].decode()}")
+            return np.frombuffer(comm[0], dtype=np.uint8).reshape(h, w, 3)
+        else:
+            return None
+
+
+class AV1(FFMPEG):
+
+    def __init__(self, **kwargs):
+        super(AV1, self).__init__(**kwargs)
+        self.codec = "libaom-av1"
+        self.additional_output_commands = ["-b:v", "0", "-strict", "experimental"]
+
+    def available(self) -> bool:
+        return super(AV1, self)._available('libaom-av1')
+
+    def _quality_param(self, quality: int) -> List[str]:
+        return ["-crf", f"{quality}"]
 
     def quality_steps(self):
         return [q for q in range(60, 0, -1)]
+
+
+class X265(FFMPEG):
+
+    def __init__(self, format: str = 'hevc', **kwargs):
+        super(X265, self).__init__(**kwargs)
+        self.format = format
+        self.codec = "libx265"
+        self.additional_output_commands = ["-b:v", "0", "-strict", "experimental"]
+
+    def available(self) -> bool:
+        return super(X265, self)._available(self.codec)
+
+    def _quality_param(self, quality: int) -> List[str]:
+        return ["-crf", f"{quality}"]
+
+    def quality_steps(self):
+        return [q for q in range(51, 0, -1)]
 
 
 codecs = {'bpg': BPG, 'webp': WebP, 'jpegfi': JPEGFI, 'av1': AV1, 'jpeg': JPEG, 'x265': X265}
