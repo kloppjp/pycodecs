f4b67d956e7e31fab3b469df5dfc2c0a3df537ba
Jan <kloppjp@gmail.com>
2020-06-17 16:00:53 +0800

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pycodecs",
-    version="0.2.0",
+    version="0.2.1",
     author="Jan",
     description="A simple python library to interface codecs",
     url="https://github.com/kloppjp/pycodecs",
